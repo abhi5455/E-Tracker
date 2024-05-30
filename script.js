a3721288a5cec3b2ec69b5575da7810e546d81ed
abhir <abhiramashok1062004@gmail.com>
2024-05-30 22:39:16 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+threelines=document.getElementById('threelines');
+threelines.addEventListener('click', function(){
+    container2=document.getElementById('container2');
+    container2.innerHTML="";
+})
---
+++ scriptCalendar.js
@@ -0,0 +1,187 @@
+let calendarYear;
+let calendarMonth;
+let calendarDay;
+let dateUpdate=document.getElementById('Date');
+let currentClickDate;
+let prevClickDate=document.getElementById('1');
+
+let headerMonth=dateUpdate.textContent.split(" ")[0];
+let headerYear=dateUpdate.textContent.split(", ")[1];
+
+today=new Date();
+currentYear=today.getFullYear();
+currentMonth=today.getMonth()+1;
+currentday=today.getDate();
+window.addEventListener('load',function () {
+    dateUpdate.textContent = monthSequence[currentMonth]+" "+currentday+", "+currentYear.toString();
+    headerMonth=dateUpdate.textContent.split(" ")[0];
+    headerYear=dateUpdate.textContent.split(", ")[1];
+    setDate(currentMonth, currentYear);
+    for(let i=1;i<=42;i++){
+        if(document.getElementById(i.toString()).textContent===currentday.toString()){
+            document.getElementById(i.toString()).click();
+            break;
+        }
+    }
+})
+
+//When today tag is Clicked
+document.getElementById('todayTag').addEventListener('click', function(){
+    setDate(currentMonth, currentYear);
+    dateUpdate.textContent = monthSequence[currentMonth]+" "+currentday+", "+currentYear.toString();
+    currentClickDate.style.backgroundColor= "white";
+    for(let i=1;i<=42;i++){
+        if(document.getElementById(i.toString()).textContent===currentday.toString()){
+            document.getElementById(i.toString()).click();
+            break;
+        }
+    }
+})
+
+
+let header=document.getElementById('month-year');
+function setDate(currentMonth, currentYear){
+    console.log("ByMain "+currentMonth, currentYear);
+
+    let column = zellersCongruence(1,currentMonth, currentYear);
+
+    let day = column===0?7:column;
+    console.log("first day "+day);
+    for (let i=1;i<=42;i++){
+        document.getElementById(i.toString()).textContent="";
+        document.getElementById(i.toString()).addEventListener('mouseenter',function(){
+            document.getElementById(i.toString()).style.backgroundColor="white";
+        });
+    }
+    let limit=daySequence[currentMonth];
+    if(((currentYear % 4 === 0 && currentYear % 100 !== 0) || (currentYear % 400 === 0)) && currentMonth===2){
+        console.log(currentMonth);
+        limit=29;
+    }
+    for(let i=1;i<=limit;i++)
+    {
+        let firstDay=document.getElementById(day.toString());
+        firstDay.textContent=i.toString();
+        firstDay.addEventListener('mouseenter',function () {
+            firstDay.style.backgroundColor="lightgrey";
+            if((headerMonth!==calendarMonth)&&(headerYear!==calendarYear)){
+                currentClickDate.style.backgroundColor="white";
+            }
+            else{
+                currentClickDate.style.backgroundColor="lightskyblue";
+            }
+            firstDay.style.backgroundColor="lightgrey";
+            firstDay.style.cursor="pointer";
+        })
+        firstDay.addEventListener('mouseleave',function () {
+            firstDay.style.backgroundColor="white";
+            if((headerMonth!==calendarMonth)&&(headerYear!==calendarYear)){
+                currentClickDate.style.backgroundColor="white";
+            }
+            else{
+                currentClickDate.style.backgroundColor="lightskyblue";
+            }
+        })
+        day=day+1;
+    }
+    for (let i=day;i<=42;i++){
+        let firstDay=document.getElementById(i.toString());
+        firstDay.style.backgroundColor="white";
+    }
+    header.textContent=currentYear.toString()+" "+monthSequence[currentMonth];
+    calendarYear=currentYear;
+    calendarMonth=monthSequence[currentMonth];
+    console.log(calendarYear+calendarMonth);
+}
+function zellersCongruence(day,month,year){
+    if(month<3){
+        month+=12;
+        year--;
+        console.log(month,year);
+    }
+    let K = year % 100;
+    let J = Math.floor(year / 100);
+    let column = (day + Math.floor((13 * (month + 1)) / 5) + K + Math.floor(K / 4) + Math.floor(J / 4) + (5 * J)) % 7;
+    return column;
+}
+
+//changing Calendar when left or right arrow is clicked
+const monthSequence=['','JANUARY','FEBRUARY','MARCH','APRIL','MAY','JUNE','JULY','AUGUST','SEPTEMBER','OCTOBER','NOVEMBER','DECEMBER'];
+const daySequence=[0,31,28,31,30,31,30,31,31,30,31,30,31]
+let calendarPrev=document.getElementById('prev');
+let calendarNext=document.getElementById('next');
+calendarPrev.addEventListener('click',changeCalendarLeft);
+calendarNext.addEventListener('click',changeCalendarRight);
+
+function changeCalendarLeft(){
+    let headerText=document.getElementById('month-year').textContent;
+    const year_month=headerText.split(" ");
+    let monthNo=1;
+    for(let i=1;i<=monthSequence.length;i++) {
+        if (monthSequence[i] === year_month[1]) {
+            monthNo = i;
+            break;
+        }
+    }
+    let year=parseInt(year_month[0]);
+    if(monthNo===1){
+        year=year-1;
+    }
+    monthNo=monthNo-1===0?12:monthNo-1;
+    setDate(monthNo,year);
+    //click update
+    if((headerMonth!==calendarMonth)&&(headerYear!==calendarYear)){
+        console.log(headerMonth+calendarMonth+headerYear+calendarYear)
+        currentClickDate.style.backgroundColor="white";
+    }
+    else{
+        currentClickDate.style.backgroundColor="lightskyblue";
+    }
+}
+function changeCalendarRight(){
+    let headerText=document.getElementById('month-year').textContent;
+    const year_month=headerText.split(" ");
+    let monthNo=1;
+    for(let i=1;i<=monthSequence.length;i++) {
+        if (monthSequence[i] === year_month[1]) {
+            monthNo = i;
+            break;
+        }
+    }
+    let year=parseInt(year_month[0]);
+    if(monthNo===12){
+        year=year+1;
+    }
+    monthNo=monthNo+1===13?1:monthNo+1;
+    setDate(monthNo,year);
+    //click update
+    if((headerMonth!==calendarMonth)&&(headerYear!==calendarYear)){
+        console.log(headerMonth+calendarMonth+headerYear+calendarYear)
+        currentClickDate.style.backgroundColor="white";
+    }
+    else{
+        currentClickDate.style.backgroundColor="lightskyblue";
+    }
+}
+
+//On Date Click
+dateNumbers=document.getElementsByClassName("COLUMN");
+for(let i=1;i<=dateNumbers.length;i++){
+    dateNumbers[i].addEventListener('click',dateNumbersClick);
+}
+function dateNumbersClick(){
+    let clickedElement=event.target;
+    if(clickedElement.textContent===""){
+        return;
+    }
+    let str= calendarMonth+" "+clickedElement.textContent+", "+calendarYear.toString();
+    dateUpdate.textContent=str;
+    headerMonth=dateUpdate.textContent.split(" ")[0];
+    headerYear=dateUpdate.textContent.split(", ")[1];
+    currentClickDate=clickedElement;
+    currentClickDate.style.backgroundColor="lightskyblue";
+    if(prevClickDate!==currentClickDate)
+        prevClickDate.style.backgroundColor="white";
+    prevClickDate=document.getElementById(clickedElement.id);
+    console.log(str);
+}
