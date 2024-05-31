0584c1e3a5ee713b96cc1fa64d4e6ee243d1f77c
abhir <abhiramashok1062004@gmail.com>
2024-05-31 13:38:26 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-threelines=document.getElementById('threelines');
+let compute=document.getElementById('compute');
+let from=document.getElementById('from');
+let to=document.getElementById('to');
+let days=document.getElementById('days');
+let expenses=document.getElementById('expense');
+
+let showEstimate=document.getElementById('showEstimate');
+compute.addEventListener('click',function(){
+    from.textContent="From: "+userSelectDate;
+    to.textContent="To: "+userSelectDate;
+    days.textContent="Days: 1 day";
+    showEstimate.style.display="block";
+    console.log(userSelectDate);
+})
+
+let threelines=document.getElementById('threelines');
 threelines.addEventListener('click', function(){
-    container2=document.getElementById('container2');
+    let container2=document.getElementById('container2');
     container2.innerHTML="";
 })
