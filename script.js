3f1c5b5dd7c36f2549e6f1f86132d7efc1d8a006
abhir <abhiramashok1062004@gmail.com>
2024-06-01 23:19:24 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ let compute=document.getElementById('compute');
 let from=document.getElementById('from');
 let to=document.getElementById('to');
 let days=document.getElementById('days');
-let expenses=document.getElementById('expense');
 let container2=document.getElementById('container2');
 let container1=document.getElementById('container1');
 let mainHeader=document.getElementById('mainHeader');
 
+
+let amountExpense="";
+let nameId="";
+let description="";
+let flag=false;
+
+//Compute
 let showEstimate=document.getElementById('showEstimate');
 compute.addEventListener('click',function(){
     from.textContent="From: "+userSelectDate;
@@ -17,6 +23,7 @@ compute.addEventListener('click',function(){
 
 })
 
+
 function addExpense(){
     let expense=document.getElementById('expenseMsg');
     let newExpense=expense.cloneNode(true);
@@ -24,13 +31,25 @@ function addExpense(){
     let lastElement=document.getElementById('addNew');
     /*Need to edit*/newExpense.id="expense";
     container2.insertBefore(newExpense,lastElement);
+    let expenses=document.getElementsByClassName('expense');
+    for(let i=0;i<expenses.length;i++) {
+        expenses[i].addEventListener("click", function (event) {
+            let clickedExpense = event.currentTarget;
+            let nameId=clickedExpense.querySelector('#label1').textContent;
+            let amount=clickedExpense.querySelector('#label2').textContent;
+            openPopup(nameId,amount," ",true);
+
+        });
+    }
 }
+
 
 let plusButton=document.getElementById('plus');
 plusButton.addEventListener('click',function(){
 
-    openPopup();
+    openPopup(nameId, amountExpense, description,flag);
 })
+openPopup(nameId, amountExpense, description,flag);
 
 
 //setting info icon
@@ -52,7 +71,7 @@ threelines.addEventListener('click', function(){
     container2.innerHTML="";
 })
 
-function openPopup() {
+function openPopup(nameId, amountExpense, description,flag) {
     document.body.style.backgroundColor="black";
     container2.style.opacity=".6";
     container1.style.opacity=".6";
@@ -76,7 +95,7 @@ function openPopup() {
     newFrame.style.position = 'absolute';
     newFrame.style.right = '18vw';
 
-    console.log(container2.offsetWidth);
+
     newFrame.style.top =  '100px';
     newFrame.style.borderRadius = '40px';
     newFrame.style.border = 'none';
@@ -89,8 +108,8 @@ function openPopup() {
         alertDate.textContent = alertDateFormat;
         let okButton=newFrameDocument.getElementById('ok');
         let cancelButton=newFrameDocument.getElementById('cancel');
+        let okCancelButton=newFrameDocument.getElementById('okCancel');
         okButton.addEventListener('click', function(){
-            console.log("esdtrgh");
             addExpense();
             revertBody();
             document.body.removeChild(newFrame);
@@ -100,6 +119,60 @@ function openPopup() {
             document.body.removeChild(newFrame);
         })
 
+        newFrameDocument.getElementById('amount').value = amountExpense;
+        newFrameDocument.getElementById('name').value = nameId;
+        newFrameDocument.getElementById('description').value = description;
+
+        window.addEventListener('resize',setPosition);
+        setPosition();
+        function setPosition(){
+            if(window.matchMedia("(min-width: 1461px)").matches){
+                newFrame.style.right = '18vw';
+            }
+            else if(window.matchMedia("(max-width: 1461px)").matches) {
+                newFrame.style.right = '18vw';
+                container2.style.height = '81vh';
+            }
+            else if(window.matchMedia("(max-width: 1320px)").matches) {
+                newFrame.style.right = '16.5vw';
+                container2.style.height = '83vh';
+            }
+            else if(window.matchMedia("(max-width: 1151px)").matches) {
+                newFrame.style.right = '14vw';
+            }
+            else if(window.matchMedia("(max-width: 1020px)").matches) {
+                newFrame.style.right = '13.5vw';
+                newFrame.style.top =  '97px';
+                container2.style.height = '84vh';
+            }
+            else if(window.matchMedia("(max-width: 955px)").matches) {
+                newFrame.style.top =  '95px';
+                newFrame.style.right = '11.5vw';
+            }
+            else if(window.matchMedia("(max-width: 820px)").matches) {
+                newFrame.style.left = '45vw';
+            }
+        }
+        if(flag===true){
+            newFrameDocument.getElementById('amount').style.fontWeight='bold';
+            newFrameDocument.getElementById('amount').style.color='blue';
+            newFrameDocument.getElementById('amount').style.fontSize='x-large';
+            newFrameDocument.getElementById('name').style.color='blue';
+            newFrameDocument.getElementById('name').style.fontWeight='bold';
+            newFrameDocument.getElementById('name').style.fontSize='x-large';
+            newFrameDocument.getElementById('description').style.color='blue';
+            newFrameDocument.getElementById('description').style.fontWeight='bold';
+            newFrameDocument.getElementById('description').style.fontSize='x-large';
+            newFrameDocument.getElementById('ok').style.display='none';
+            newFrameDocument.getElementById('cancel').style.display='none';
+            newFrameDocument.getElementById('okCancel').style.display='block';
+            newFrameDocument.body.style.pointerEvents="none";
+            okCancelButton.style.pointerEvents="auto";
+            okCancelButton.addEventListener('click', function(){
+                revertBody();
+                document.body.removeChild(newFrame);
+            })
+        }
     }
 }
 function revertBody(){
