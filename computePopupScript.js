fbdf7f973d9f9dc0c4bda145a9ab175a319abe51
abhir <abhiramashok1062004@gmail.com>
2024-06-04 16:01:06 +0530

    Initial Commit

--- computePopupScript.js
+++ computePopupScript.js
@@ -1,5 +1,6 @@
 let computePopup=document.getElementById('computePopup');
 let popupCancelButton=document.getElementById('popupCancelButton');
+let popupComputeButton=document.getElementById('popupComputeButton');
 let startDate=document.getElementById('start-date');
 let endDate=document.getElementById('end-date');
 
@@ -22,6 +23,24 @@ function blurBody(){
 popupCancelButton.addEventListener('click',function(){
     revertBody();
     computePopup.style.display="none";
-    console.log(startDate.value,endDate.value.clear)
 });
+popupComputeButton.addEventListener('click',function(){
+    from.textContent=" \u00A0 From: \u00A0\u00A0\u00A0"+startDate.value;
+    to.textContent=" \u00A0 To: \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0"+endDate.value;
+    days.textContent="\u00A0 No of Days: \u00A0\u00A01";
+    showEstimate.style.display="block";
+    revertBody();
+    computePopup.style.display="none";
+    console.log(userSelectDate);
+})
+
 window.onresize=computePopupSetPos;
+
+//Compute
+let showEstimate=document.getElementById('showEstimate');
+compute.addEventListener('click',function(){
+    computePopupSetPos();
+    showEstimate.style.display="none";
+    computePopup.style.display='flex';
+    blurBody();
+})
