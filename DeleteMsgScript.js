af60330b56bfadac9babed00ce9d7a5c3e941290
abhir <abhiramashok1062004@gmail.com>
2024-06-06 14:32:47 +0530

    Initial Commit

---
+++ DeleteMsgScript.js
@@ -0,0 +1,20 @@
+function askToDelete(){
+    var deleteMsg=document.createElement("iframe");
+    deleteMsg.src='DeleteMsg.html';
+    document.body.appendChild(deleteMsg);
+    deleteMsg.width = '400px';
+    deleteMsg.height = '210';
+    deleteMsg.style.position = 'absolute';
+    deleteMsg.style.top='220px';
+    deleteMsg.style.borderRadius='20px';
+    deleteMsg.style.border='none';
+    deleteMsg.style.outline='2px solid black';
+
+    let leftPos=((container1.offsetWidth)+((container2.offsetWidth-400))/2);
+    deleteMsg.style.left=leftPos.toString()+'px';
+    window.addEventListener('resize',function(){
+        let leftPos=((container1.offsetWidth)+((container2.offsetWidth-400))/2);
+        deleteMsg.style.left=leftPos.toString()+'px';
+    })
+}
+askToDelete();
