19eb17be0664bb4eb984758071b278f0f1b756b6
abhir <abhiramashok1062004@gmail.com>
2024-06-08 11:36:44 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -266,5 +266,5 @@ function revertBody(){
 addExpense("FoodExpense","1000","Food","Spent During Dinner with Friends");
 addExpense("Outing","2500","Fun","Alappuzha Beach");
 addExpense("Groceries","500","MonthlyExpense","Home");
-addExpense("Vegetables","1200","MonthlyExpense","Home");/*
+addExpense("Vegetables","1200","","Home");/*
 openPopup("Food","1000","efwygidcsuhcb",true);/**/
