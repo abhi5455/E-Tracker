a2c0049d3af19bbd98afeb6bcad5c96e66adfdc2
abhir <abhiramashok1062004@gmail.com>
2024-06-13 14:22:35 +0530

    Initial Commit

--- executingFile.js
+++ executingFile.js
@@ -3,11 +3,13 @@ let x=1;
 function clearContainer2(){
     let children=container2.children;
     let n=children.length;
+    let j=0;
     for(let i=0;i<n;i++){
-        if((children[i].id!=="expenseMsg")&&(children[i].id!=="addNew")&&(children[i].id!=="noExpenseText")){
-            container2.removeChild(children[i]);
-            i--; //To avoid skipping during removeChild()
+        if((children[j].id!=="expenseMsg")&&(children[j].id!=="addNew")&&(children[j].id!=="noExpenseText")){
+            container2.removeChild(children[j]);
+            j--; //To avoid skipping during removeChild()
         }
+        j++;
     }
 }
 
