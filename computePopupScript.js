428a50eddca6141baae9ed700018725903c9b276
abhir <abhiramashok1062004@gmail.com>
2024-06-22 13:38:52 +0530

    Initial Commit

--- computePopupScript.js
+++ computePopupScript.js
@@ -11,16 +11,6 @@ function computePopupSetPos() {
     //computePopup.style.left = (((container1.offsetWidth+container2.offsetWidth - 350) / 2) - 2).toString() + "px";
     console.log(startDate.value,endDate.value);
 }
-function blurBody(){
-    document.body.style.backgroundColor="black";
-    container2.style.opacity=".4";
-    container1.style.opacity=".6";
-    mainHeader.style.opacity=".6";
-    mainHeader.style.outline="none";
-    mainHeader.style.pointerEvents="none";
-    container1.style.pointerEvents="none";
-    container2.style.pointerEvents="none";
-}
 popupCancelButton.addEventListener('click',function(){
     revertBody();
     computePopup.style.display="none";
