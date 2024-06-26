97777ad7d1efe5b8fcecda7ebaaf4085bfd7af31
abhir <abhiramashok1062004@gmail.com>
2024-06-26 20:17:18 +0530

    Initial Commit

--- DeleteMsgScript.js
+++ DeleteMsgScript.js
@@ -1,59 +1,42 @@
-function askToDelete(a){
-    const deleteMsg=document.createElement("iframe");
-    deleteMsg.src='DeleteMsg.html';
-    deleteMsg.width = '360px';
-    deleteMsg.height = '200';
-    deleteMsg.style.position = 'absolute';
-    deleteMsg.style.top='220px';
-    deleteMsg.style.borderRadius='25px';
-    deleteMsg.style.border='none';
-    let text="Do you want to delete "+"'"+a+"'";
+function askToDelete(a) {
+    const deleteMsg = document.getElementById('deleteBlock');
+    let text = "Do you want to delete " + "'" + a + "'";
 
-
-    let leftPos=((container1.offsetWidth)+((container2.offsetWidth-360))/2);
-    deleteMsg.style.left=leftPos.toString()+'px';
-    window.addEventListener('resize',function(){
-        let leftPos=((container1.offsetWidth)+((container2.offsetWidth-360))/2);
-        deleteMsg.style.left=leftPos.toString()+'px';
+    let leftPos = ((container1.offsetWidth) + ((container2.offsetWidth - 360)) / 2);
+    deleteMsg.style.left = leftPos.toString() + 'px';
+    window.addEventListener('resize', function () {
+        let leftPos = ((container1.offsetWidth) + ((container2.offsetWidth - 360)) / 2);
+        deleteMsg.style.left = leftPos.toString() + 'px';
         resizeDeleteMsg();
     })
-    deleteMsg.onload = function() {
-        deleteMsg.contentDocument.getElementById('warningMsg').textContent=text;
-        deleteMsg.contentDocument.getElementById('x').addEventListener('click', function(){
-            document.body.removeChild(deleteMsg);
-            revertBody();
-        });
-        deleteMsg.contentDocument.getElementById('no').addEventListener('click', function(){
-            document.body.removeChild(deleteMsg);
-            revertBody();
-        });
-        deleteMsg.contentDocument.getElementById('yes').addEventListener('click', function(){
-            deleteExpense();
-            document.body.removeChild(deleteMsg);
-            revertBody();
-        })
+    document.getElementById('warningMsg').textContent = text;
 
-    }
-
+    document.getElementById('x').addEventListener('click', function () {
+        deleteMsg.style.display = "none";
+        revertBody();
+    });
+    document.getElementById('no').addEventListener('click', function () {
+        deleteMsg.style.display = "none";
+        revertBody();
+    });
+    document.getElementById('yes').addEventListener('click', function () {
+        deleteExpense();
+        deleteMsg.style.display = "none";
+        revertBody();
+    })
     blurBody();
+    deleteMsg.style.display = "block";
     resizeDeleteMsg();
-    document.body.appendChild(deleteMsg);
-    
-    function resizeDeleteMsg(){
-        const mediaQuery =window.matchMedia("(max-width: 690px)")
-        if(mediaQuery.matches) {
-            leftPos=((container2.offsetWidth-360)/2)-5;
+
+    function resizeDeleteMsg() {
+        const mediaQuery = window.matchMedia("(max-width: 690px)")
+        if (mediaQuery.matches) {
+            leftPos = ((container2.offsetWidth) / 2) - 180;
             deleteMsg.style.left = leftPos.toString() + 'px';
-            deleteMsg.style.position="fixed";
-            let topPos=210;
-            deleteMsg.style.top=topPos.toString()+'px';
-        }
-        else{
-            leftPos=((container1.offsetWidth)+((container2.offsetWidth-360))/2);
-            deleteMsg.style.left=leftPos.toString()+'px';
-            deleteMsg.width = '360px';
-            deleteMsg.style.position = 'absolute';
-            deleteMsg.style.top='220px';
+            deleteMsg.style.position = "fixed";
+        } else {
+            leftPos = ((container1.offsetWidth) + ((container2.offsetWidth - 360)) / 2);
+            deleteMsg.style.left = leftPos.toString() + 'px';
         }
     }
 }
