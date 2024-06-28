9170f2b920688b533a213d1f9275627631c11a9c
abhir <abhiramashok1062004@gmail.com>
2024-06-28 14:23:20 +0530

    Initial Commit

--- Login/Login.js
+++ Login/Login.js
@@ -9,8 +9,7 @@ let password=document.getElementById("password");
 let phoneNumber=document.getElementById("phoneNumber");
 let phoneNumberBlock=document.getElementById("phoneNumberBlock");
 let signInAlternate=document.getElementById("signInAlternate");
-
-//userInteract.removeChild(name);
+let vp1=document.getElementById("vp1");
 
 signIn.addEventListener("click", function(){
     if(signInFlag){
@@ -61,3 +60,30 @@ signInAlternate.addEventListener("click", function(){
         emailLogInFlag=true;
     }
 })
+
+vp1.style.height=document.body.style.height+"px";
+console.log(vp1.offsetHeight);
+
+
+let sampleImages=document.getElementsByClassName("sampleImages");
+let Img=document.getElementById("img");
+window.addEventListener('resize',function(){
+    sampleImagesResize();
+})
+
+function sampleImagesResize() {
+    for (let i = 0; i < sampleImages.length; i++) {
+        if (window.matchMedia('(max-width: 600px)').matches) {
+            document.getElementById('sampleImage1').style.paddingTop="100px";
+            sampleImages[i].style.height = (Img.offsetHeight - 60) + "px"
+        }
+        else if(window.matchMedia('(max-width: 900px)').matches) {
+            sampleImages[i].style.height = (Img.offsetHeight - 100) + "px"
+        }
+        else{
+            sampleImages[i].style.height = (Img.offsetHeight - 50) + "px"
+        }
+    }
+}
+sampleImagesResize();
+
