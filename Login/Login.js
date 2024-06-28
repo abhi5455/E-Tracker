086e7509b1b43954b036bdeccd9e59da3d924159
abhir <abhiramashok1062004@gmail.com>
2024-06-28 14:26:51 +0530

    Initial Commit

--- Login/Login.js
+++ Login/Login.js
@@ -61,29 +61,3 @@ signInAlternate.addEventListener("click", function(){
     }
 })
 
-vp1.style.height=document.body.style.height+"px";
-console.log(vp1.offsetHeight);
-
-
-let sampleImages=document.getElementsByClassName("sampleImages");
-let Img=document.getElementById("img");
-window.addEventListener('resize',function(){
-    sampleImagesResize();
-})
-
-function sampleImagesResize() {
-    for (let i = 0; i < sampleImages.length; i++) {
-        if (window.matchMedia('(max-width: 600px)').matches) {
-            document.getElementById('sampleImage1').style.paddingTop="100px";
-            sampleImages[i].style.height = (Img.offsetHeight - 60) + "px"
-        }
-        else if(window.matchMedia('(max-width: 900px)').matches) {
-            sampleImages[i].style.height = (Img.offsetHeight - 100) + "px"
-        }
-        else{
-            sampleImages[i].style.height = (Img.offsetHeight - 50) + "px"
-        }
-    }
-}
-sampleImagesResize();
-
